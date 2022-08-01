6ae1376b3c55acc12791b17a2d83eefbdbc1b110
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-08-01 11:39:20 -0300

    refactor: reorganizes app

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -3,25 +3,13 @@ import styles from './Todo.module.css'
 
 export function ToDo() {
   return (
-    <div className={styles.content}>
-      <header className={styles.header}>
-          <div className={styles.headerContent}>
-            <li className={styles.tasks}>Tarefas criadas</li>
-            <li className={styles.tasksCounter}>0</li>
-          </div>
-          <div className={styles.headerContent}>
-            <li className={styles.doneTasks}>Concluídas</li>
-            <li className={styles.doneTasksCounter}>0</li>
-          </div>
-      </header>
-      <div className={styles.taskBox}>
+    <div className={styles.taskBox}>
         <button>
           <Circle size={18} className={styles.checkbox}/>
           <CheckCircle size={18} weight="fill" className={styles.checkedCheckbox}/>
         </button>
         <p className={styles.taskText}>Iniciar o desenvolvimento das funcionalidades do desafio.</p>
         <button><Trash size={16} className={styles.trashIcon}/></button>
-      </div>
     </div>
   )
 }
