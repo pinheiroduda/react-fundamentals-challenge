3aac91a044ac82c8597e0f57bf4a66590373e931
Eduarda Pinheiro <eduardapinheiro1302@gmail.com>
2022-08-01 14:00:00 -0300

    feat: displays check icon according to state

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,12 +1,18 @@
 import { CheckCircle, Circle, Trash } from 'phosphor-react'
+import { useState } from 'react'
 import styles from './Todo.module.css'
 
 export function ToDo() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  function handleCheck() {
+    setIsChecked(!isChecked);
+  }
+
   return (
     <div className={styles.taskBox}>
-        <button>
-          <Circle size={18} className={styles.checkbox}/>
-          <CheckCircle size={18} weight="fill" className={styles.checkedCheckbox}/>
+        <button onClick={handleCheck}>
+          {isChecked ? <CheckCircle size={18} weight="fill" className={styles.checkedCheckbox}/> : <Circle size={18} className={styles.checkbox}/>}
         </button>
         <p className={styles.taskText}>Iniciar o desenvolvimento das funcionalidades do desafio.</p>
         <button><Trash size={16} className={styles.trashIcon}/></button>
